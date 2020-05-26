ff56868e1bbd9dad417b40f95792ec0e5c8ec903
maibangmin <mbm1216@163.com>
2020-05-26 22:26:53 +0800

    Create MiPhone.java

---
+++ src/factory/abstractF/MiPhone.java
@@ -0,0 +1,13 @@
+package factory.abstractF;
+
+public class MiPhone implements Phone {
+
+    public MiPhone() {
+        this.make();
+    }
+
+    @Override
+    public void make() {
+        System.out.println("Make a XiaoMi Phone");
+    }
+}
