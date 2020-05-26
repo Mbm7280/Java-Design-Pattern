d1d1c47fb27e60083dae1daf8086b1864d5e845a
maibangmin <mbm1216@163.com>
2020-05-26 22:26:38 +0800

    Create ApplePhone.java

---
+++ src/factory/FMethod/ApplePhone.java
@@ -0,0 +1,13 @@
+package factory.FMethod;
+
+public class ApplePhone implements Phone {
+
+    public ApplePhone() {
+        this.make();
+    }
+
+    @Override
+    public void make() {
+        System.out.println("Make a Apple Phone");
+    }
+}
