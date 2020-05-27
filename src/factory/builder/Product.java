886ad3025fe52e14facaaa56218dab6158c732f6
maibangmin <mbm1216@163.com>
2020-05-27 21:49:36 +0800

    Create Product.java

---
+++ src/factory/builder/Product.java
@@ -0,0 +1,37 @@
+package factory.builder;
+
+public class Product {
+
+    // 地基
+    private String basic;
+
+    // 围墙
+    private String wall;
+
+    // 屋顶
+    private String roof;
+
+    public String getBasic() {
+        return basic;
+    }
+
+    public void setBasic(String basic) {
+        this.basic = basic;
+    }
+
+    public String getWall() {
+        return wall;
+    }
+
+    public void setWall(String wall) {
+        this.wall = wall;
+    }
+
+    public String getRoof() {
+        return roof;
+    }
+
+    public void setRoof(String roof) {
+        this.roof = roof;
+    }
+}
