b3e6e980b09989af9ea73ba48f124007cf01ebd3
maibangmin <mbm1216@163.com>
2020-05-27 22:05:15 +0800

    Create Builder.java

---
+++ src/builder/Builder.java
@@ -0,0 +1,18 @@
+package builder;
+
+public interface Builder {
+
+
+    // 建造地基
+    public void buildBasic();
+
+    // 建造围墙
+    public void buildWall();
+
+    // 建造屋顶
+    public void buildRoof();
+
+    // 建造房子
+    public Product buildHouse();
+
+}
