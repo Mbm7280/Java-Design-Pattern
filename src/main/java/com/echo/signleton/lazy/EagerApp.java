87d7fa2b119e664c171cba7b9316585bf96eb7ec
Echo <56158949+Mbm7280@users.noreply.github.com>
2025-03-23 22:24:55 +0800

    Create EagerApp.java
    day01：singleton

---
+++ src/main/java/com/echo/signleton/lazy/EagerApp.java
@@ -0,0 +1,30 @@
+package com.echo.signleton.lazy;
+
+/****************************************************
+ * 创建人：Echo
+ * 创建时间: 2025/3/23 21:43
+ * 项目名称: {Java-Design-Pattern}
+ * 文件名称: EagerApp
+ * 文件描述: [
+ *      设计模式：单例设计模式
+ *      饿汉式
+ *      特性：不需要加锁，执行效率提高。类加载时就会初始化，浪费内存
+ *      线程：安全
+ *      建议使用，如果需要 懒加载 则推荐使用 StaticInnerApp 中的静态内部类方式
+ *
+ * ]
+ * version：1.0
+ *
+ ********************************************************/
+public class EagerApp {
+
+    private static EagerApp uniqueInstance = new EagerApp();
+
+    private EagerApp() {
+    }
+
+    public static EagerApp getUniqueInstance() {
+        return uniqueInstance;
+    }
+
+}
