cd933c1e0d79da5317c150b69cadcd1c03ff72f2
Echo <56158949+Mbm7280@users.noreply.github.com>
2025-03-26 09:44:39 +0800

    day02-建造者设计模式注释提交

--- src/main/java/com/echo/builder/DemoTest.java
+++ src/main/java/com/echo/builder/DemoTest.java
@@ -1,5 +1,17 @@
 package com.echo.builder;
 
+/****************************************************
+ * 创建人：Echo
+ * 创建时间: 2025/3/25 21:42
+ * 项目名称: {Java-Design-Pattern}
+ * 文件名称: DemoTest
+ * 文件描述: [
+ *      设计模式：建造者设计模式
+ *      特性：使用多个简单对象构建出一个复杂对象
+ * ]
+ * version：1.0
+ *
+ ********************************************************/
 public class DemoTest {
 
     public static void main(String[] args) {
